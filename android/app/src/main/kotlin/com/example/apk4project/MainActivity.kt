d146d8c0f5f200561c37ec0080edf799c9cf3a5b
mdshahilak <mdshahilak@gmail.com>
2022-04-28 10:31:24 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/apk4project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apk4project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            children: [
+              Container(
+                width: 100,
+                height: double.infinity,
+                color: Colors.red,
+              ),
+              SizedBox(
+                width: 45,
+              ),
+              Container(
+
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.yellow,
+                        ),
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.green,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              SizedBox(
+                width: 45,
+              ),
+              Container(
+                width: 100,
+                height: double.infinity,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
